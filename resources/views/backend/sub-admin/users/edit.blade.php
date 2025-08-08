a89327e801acdf80cc6fe7b6d107d0e882051714
tuantrung128 <tuantrunglc@gmail.com>
2025-08-08 07:54:46 +0700

    git pull

--- resources/views/backend/sub-admin/users/edit.blade.php
+++ resources/views/backend/sub-admin/users/edit.blade.php
@@ -47,23 +47,6 @@
           <span class="text-danger">{{$message}}</span>
           @enderror
         </div>
-        @php 
-        $roles=DB::table('users')->select('role')->where('id',$user->id)->get();
-        // dd($roles);
-        @endphp
-        <div class="form-group">
-            <label for="role" class="col-form-label">Role</label>
-            <select name="role" class="form-control">
-                <option value="">-----Select Role-----</option>
-                @foreach($roles as $role)
-                    <option value="{{$role->role}}" {{(($role->role=='admin') ? 'selected' : '')}}>Admin</option>
-                    <option value="{{$role->role}}" {{(($role->role=='user') ? 'selected' : '')}}>User</option>
-                @endforeach
-            </select>
-          @error('role')
-          <span class="text-danger">{{$message}}</span>
-          @enderror
-          </div>
           <div class="form-group">
             <label for="status" class="col-form-label">Status</label>
             <select name="status" class="form-control">
