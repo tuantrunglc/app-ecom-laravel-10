3177efcd9a06b288122a41e9599eea6ae686a632
tuantrung128 <tuantrunglc@gmail.com>
2025-08-18 23:42:55 +0700

    udpate

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -38,6 +38,13 @@
       </div>
     </div>
   </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{asset('backend/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="{{asset('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
   <script>
     (function($){
       $(function(){
@@ -65,13 +72,6 @@
       $('.alert').slideUp();
     },4000);
   </script>
-  <!-- Bootstrap core JavaScript-->
-  <script src="{{asset('backend/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="{{asset('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
   <!-- Custom scripts for all pages-->
   <script src="{{asset('backend/js/sb-admin-2.min.js')}}"></script>
 
