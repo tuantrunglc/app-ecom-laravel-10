8b37bce8927eb5ee1ffee09f4a047ebf76bacf7d
tuantrung128 <tuantrunglc@gmail.com>
2025-08-18 23:42:18 +0700

    uidpate

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -38,27 +38,6 @@
       </div>
     </div>
   </div>
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="{{asset('backend/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="{{asset('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="{{asset('backend/js/sb-admin-2.min.js')}}"></script>
-
-  <!-- Page level plugins -->
-  <script src="{{asset('backend/vendor/chart.js/Chart.min.js')}}"></script>
-
-  <!-- Page level custom scripts -->
-  {{-- <script src="{{asset('backend/js/demo/chart-area-demo.js')}}"></script> --}}
-  {{-- <script src="{{asset('backend/js/demo/chart-pie-demo.js')}}"></script> --}}
-
-  @stack('scripts')
-
-  <!-- Fallback: Ensure sidebar toggle works on mobile -->
   <script>
     (function($){
       $(function(){
@@ -86,3 +65,24 @@
       $('.alert').slideUp();
     },4000);
   </script>
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{asset('backend/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="{{asset('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="{{asset('backend/js/sb-admin-2.min.js')}}"></script>
+
+  <!-- Page level plugins -->
+
+  <!-- Page level custom scripts -->
+  {{-- <script src="{{asset('backend/js/demo/chart-area-demo.js')}}"></script> --}}
+  {{-- <script src="{{asset('backend/js/demo/chart-pie-demo.js')}}"></script> --}}
+
+  @stack('scripts')
+
+  <!-- Fallback: Ensure sidebar toggle works on mobile -->
+
+  <script src="{{asset('backend/vendor/chart.js/Chart.min.js')}}"></script>
